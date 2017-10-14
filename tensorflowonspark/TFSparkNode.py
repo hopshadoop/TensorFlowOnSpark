d0fc08092c36a49711d0e6e629015c15eadd6e7a
robzor92 <robin.eric.andersson@gmail.com>
2017-10-14 13:05:47 +0200

    Store tensorboard logdir contents in HopsFS

--- tensorflowonspark/TFSparkNode.py
+++ tensorflowonspark/TFSparkNode.py
@@ -245,6 +245,7 @@ def start(fn, tf_args, cluster_info, defaultFS, working_dir, background):
 
             # package up the context for the TF node
             fn(tf_args, ctx)
+
             logging.info("Finished TensorFlow {0}:{1} on cluster node {2}".format(job_name, task_index, worker_num))
 
         return [(worker_num, job_name, task_index)]
@@ -459,6 +460,10 @@ def run(fn, tf_args, cluster_meta, tb, queues, app_id, background):
             # otherwise, just run TF function in the main executor/worker thread
             logging.info("Starting TensorFlow {0}:{1} on cluster node {2} on foreground thread".format(job_name, task_index, worker_num))
             fn(tf_args, ctx)
+            if gpus_are_present_on_executors and gpu_present and job_name == 'worker' and task_index == 0:
+                tensorboard.store()
+            elif not gpus_are_present_on_executors and job_name == 'worker' and task_index == 0:
+                tensorboard.store()
             logging.info("Finished TensorFlow {0}:{1} on cluster node {2}".format(job_name, task_index, worker_num))
 
     return _mapfn
