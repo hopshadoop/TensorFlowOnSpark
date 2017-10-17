0f10a0a14cb79ed7088e05df888168c2a4ea9655
robzor92 <robin.eric.andersson@gmail.com>
2017-10-17 16:02:53 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name = 'tfspark',
   packages = ['tensorflowonspark'],
-  version = '1.1.9',
+  version = '1.2.0',
   description = 'Deep learning with TensorFlow on Apache Spark clusters',
   author = 'Yahoo, Inc.',
   url = 'https://github.com/hopshadoop/TensorFlowOnSpark',
